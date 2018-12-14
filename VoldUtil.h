c9c8e3034bbc2cc565429e2caa89f5cbe3231b2b
Neeraj Soni <neersoni@codeaurora.org>
2018-12-14 15:18:15 +0530

    system: vold: Use wrapped key for metadata encryption
    Wrapped key feature is needed for better security of encryption keys and to
    ensure data integrity when crypto key cache is cleared during reset operation
    of storage/crypto hardware.
    
    Original patch: https://source.codeaurora.org/quic/la/platform/system/vold/commit/?h=LA.QSSI.11.0.r1-05600-qssi.0&id=c480f913e6abc2757c0d79afba5a3df1c4adc731
    [Pig]: Clean up all deprecated codes that were removed during latter
    merge.
    
    CRs-Fixed: 2367150
    Change-Id: I83d14861bf81e102151fa3417d84008c214a9ac0

--- VoldUtil.h
+++ VoldUtil.h
@@ -21,6 +21,7 @@
 extern android::fs_mgr::Fstab fstab_default;
 
 #define DATA_MNT_POINT "/data"
+#define METADATA_MNT_POINT "/metadata"
 
 std::string GetUserDataBlockDevicePath();
 std::string GetUfsHostControllerSysfsPath();
